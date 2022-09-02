3267c768f3afca1ba644782c2b00ba57cce16aac
Joe <spizjo01@gmail.com>
2022-09-02 09:01:13 -0600

    Added and styled header component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Components/Header';
+import '../src/index.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          The Future Home of a Purring Wolverine <code>src/App.js</code>
-        </p>
-        <p>See you soon, I can't wait to help you relax</p>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header/>
+      
+       
     </div>
   );
 }
