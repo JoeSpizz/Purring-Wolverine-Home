0c764271c503815e487818be2fee41a03639548a
Joe <spizjo01@gmail.com>
2022-09-06 11:24:36 -0600

    likes uptick properly with supabase

--- src/Components/RequestCard.js
+++ src/Components/RequestCard.js
@@ -3,23 +3,49 @@ function RequestCard ({request}){
 const [likes, setLikes] = useState(request.likes)
 function handleSubmit(){
     let id = request.id
-    fetch(`http://localhost:3000/requests/${id}`, {
-      method: "PUT",
-      headers: {
-        'Accept': 'application/json',
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin":'http://localhost:3001',
-      },
-      body: JSON.stringify({
-        id: request.id,
-        name: request.name,
-        idea: request.idea,
-        genre: request.genre,
-        likes: likes +1
-      }),
-    })
-      .then((r) => r.json())
-  .then((json) => console.log("hello" +json.likes));
+    let myHeaders = {
+"apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im53Z25rdmFla2l0bnhyYmpiY2NoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjIzODY2MDMsImV4cCI6MTk3Nzk2MjYwM30.f5fnLu4pfrXFcsP4VGAdDgwGmsXM0QwKiFTKSLanst0",
+"Content-Type": "application/json",
+"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im53Z25rdmFla2l0bnhyYmpiY2NoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjIzODY2MDMsImV4cCI6MTk3Nzk2MjYwM30.f5fnLu4pfrXFcsP4VGAdDgwGmsXM0QwKiFTKSLanst0",
+    }
+let raw = JSON.stringify([
+  {
+    "id": id,
+    "name": request.name,
+        "idea": request.idea,
+       "genre": request.genre,
+        "likes": likes +1
+  }
+]);
+
+let requestOptions = {
+  method: 'PUT',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch(`https://nwgnkvaekitnxrbjbcch.supabase.co/rest/v1/Requests?id=eq.${id}`, requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+  //   fetch(`http://localhost:3000/requests/${id}`, {
+  //     method: "PUT",
+  //     headers: {
+  //       'Accept': 'application/json',
+  //       "Content-Type": "application/json",
+  //       "Access-Control-Allow-Origin":'http://localhost:3001',
+  //     },
+  //     body: JSON.stringify({
+  //       id: request.id,
+  //       name: request.name,
+  //       idea: request.idea,
+  //       genre: request.genre,
+  //       likes: likes +1
+  //     }),
+  //   })
+  //     .then((r) => r.json())
+  // .then((json) => console.log("hello" +json.likes));
     setLikes(likes+1)
 }
 
